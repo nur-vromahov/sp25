7d70bb0c36a18dbda37ad8041a19b956efb39691
vromahov <vromahov@nurtelecom.kg>
2025-02-15 09:25:49 +0600

    game logic

--- proj0/src/game2048logic/GameLogic.java
+++ proj0/src/game2048logic/GameLogic.java
@@ -1,25 +1,33 @@
 package game2048logic;
 
 import game2048rendering.Side;
+
 import static game2048logic.MatrixUtils.rotateLeft;
 import static game2048logic.MatrixUtils.rotateRight;
 
 /**
- * @author  Josh Hug
+ * @author Josh Hug
  */
 public class GameLogic {
-    /** Moves the given tile up as far as possible, subject to the minR constraint.
+    /**
+     * Moves the given tile up as far as possible, subject to the minR constraint.
      *
      * @param board the current state of the board
      * @param r     the row number of the tile to move up
-     * @param c -   the column number of the tile to move up
+     * @param c     -   the column number of the tile to move up
      * @param minR  the minimum row number that the tile can land in, e.g.
      *              if minR is 2, the moving tile should move no higher than row 2.
-     * @return      if there is a merge, returns the 1 + the row number where the merge occurred.
-     *              if no merge occurs, then return 0.
+     * @return if there is a merge, returns the 1 + the row number where the merge occurred.
+     * if no merge occurs, then return 0.
      */
     public static int moveTileUpAsFarAsPossible(int[][] board, int r, int c, int minR) {
         // TODO: Fill this in in tasks 2, 3, 4
+        int i = r - 1;
+        while (i > -1 && board[i][c] == 0) {
+            i--;
+        }
+        board[i][c] = board[r][c];
+        board[r][c] = 0;
         return 0;
     }
 
@@ -27,8 +35,8 @@ public class GameLogic {
      * Modifies the board to simulate the process of tilting column c
      * upwards.
      *
-     * @param board     the current state of the board
-     * @param c         the column to tilt up.
+     * @param board the current state of the board
+     * @param c     the column to tilt up.
      */
     public static void tiltColumn(int[][] board, int c) {
         // TODO: fill this in in task 5
@@ -38,7 +46,7 @@ public class GameLogic {
     /**
      * Modifies the board to simulate tilting all columns upwards.
      *
-     * @param board     the current state of the board.
+     * @param board the current state of the board.
      */
     public static void tiltUp(int[][] board) {
         // TODO: fill this in in task 6
